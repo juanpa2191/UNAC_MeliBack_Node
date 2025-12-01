efacda99091fac9129745c28e007fb2d6d6572ad
Juan Pablo Ospina <juanpa2191@hotmail.com>
2025-11-30 22:38:04 -0500

    modificaciones variables de ambiente

--- src/config/database.js
+++ src/config/database.js
@@ -3,13 +3,13 @@ require('dotenv').config();
 
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.DB_NAME || process.env.MYSQLDATABASE,
+  process.env.DB_USER || process.env.MYSQLUSER,
+  process.env.DB_PASSWORD || process.env.MYSQLPASSWORD,
   {
-    host: process.env.DB_HOST,
+    host: process.env.DB_HOST || process.env.MYSQLHOST,
     dialect: 'mysql',
-    port: process.env.DB_PORT,
+    port: process.env.DB_PORT || process.env.MYSQLPORT,
     logging: false,
   }
 );
