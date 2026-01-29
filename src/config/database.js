ecde496a23164dab5448498dd85399ea78772c17
Juan Pablo Ospina <juanpa2191@hotmail.com>
2026-01-28 22:01:12 -0500

    configuracion bd

--- src/config/database.js
+++ src/config/database.js
@@ -3,13 +3,13 @@ require('dotenv').config();
 
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || process.env.MYSQL_DATABASE_UNAC,
-  process.env.DB_USER || process.env.MYSQLUSER,
-  process.env.DB_PASSWORD || process.env.MYSQLPASSWORD,
+  process.env.DB_NAME || process.env.MYSQL_DATABASE_UNAC || "meli_db",
+  process.env.DB_USER || process.env.MYSQLUSER || "admin",
+  process.env.DB_PASSWORD || process.env.MYSQLPASSWORD || "M3d3ll1n.B4rb0s4",
   {
-    host: process.env.DB_HOST || process.env.MYSQLHOST,
+    host: process.env.DB_HOST || process.env.MYSQLHOST || "myapp-mysql.cdy8yyku8acu.us-east-2.rds.amazonaws.com",
     dialect: 'mysql',
-    port: process.env.DB_PORT || process.env.MYSQLPORT,
+    port: process.env.DB_PORT || process.env.MYSQLPORT || 3306,
     logging: false,
   }
 );
